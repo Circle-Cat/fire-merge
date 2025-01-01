25decd90bb2e0f3b72f0c4b2ac9b8be744f1f734
吴雨 <wuyu@u.circlecat.org>
2025-01-01 08:40:20 +0000

    [FM-5] TEST PIPELINE
    Change-Id: I122189bd96f65a76026adb8e34a66ad8ff5168d7

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  {
+    ignores: ['.pnp.cjs','.yarn/',], // 要忽略的文件或目录
+  },
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
