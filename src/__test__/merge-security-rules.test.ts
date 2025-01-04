7212c67c93432f2f4afd6118773bb18a0b85f582
吴雨 <wuyu@u.circlecat.org>
2025-01-04 09:19:40 +0000

    draft
    Change-Id: I5f3e1e11b6fb4314d3e3c156187687e0bb8c7182

--- src/__test__/merge-security-rules.test.ts
+++ src/__test__/merge-security-rules.test.ts
@@ -1,11 +1,154 @@
-import mergeSecurityRules from "../merge-security-rules";
+import fs from 'fs';
+import path from 'path';
+import {findRulesFiles,  readFileContent } from "../merge-security-rules";
 
 
-describe('Test mergeSecurityRules', () => {
-  test('Test mergeSecurityRules success', () => {
-    const expectedResult = 'example: merged security rules';
-    const actualResult = mergeSecurityRules();
+jest.mock("fs", () => ({
+  readdirSync: jest.fn(),
+  statSync: jest.fn(),
+  promises: {
+    readFile: jest.fn(),
+  },
+}));
+jest.mock('path');
 
-    expect(actualResult).toEqual(expectedResult);
+const TEST_DIR = '/mock/workspace';
+const TEST_SUB_DIR = 'subdir';
+const TEST_ROOT_SR_TEMPLATE = 'firebase.rules.template';
+const TEST_FIRST_RULES_FILE = 'file1.rules';
+const TEST_SECOND_RULES_FILE = 'file2.rules';
+const TEST_FIRST_TS_FILE = 'file3.ts';
+const TEST_SUB_DIR_PARTH = '/mock/workspace/subdir';
+const TEST_ROOT_TEMPLATE_PATH = '/mock/workspace/firebase.rules.template';
+const TEST_FIRST_RULES_FILE_PATH = '/mock/workspace/file1.rules';
+const TEST_SECOND_RULES_FILE_PATH = '/mock/workspace/file2.rules';
+const TEST_FIRST_TS_FILE_PATH = '/mock/workspace/file3.ts';
+const TEST_FIRST_SUB_RULES_FILE = 'subfile1.rules';
+const TEST_SECOND_SUB_RULES_FILE = 'subfile2.rules';
+const TEST_FIRST_SUB_RULES_FILE_PATH = '/mock/workspace/subdir/subfile1.rules';
+const TEST_SECOND_SUB_RULES_FILE_PATH = '/mock/workspace/subdir/subfile2.rules';
+const TEST_FIRST_RULES_FILE_CONTENT = `
+    match /articles/{article} {
+      allow read: if request.auth != null;
+      allow create, update: if request.auth.token.admin == true;
+    }`;
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('findRulesFiles', () => {
+  test('there are multiple SR templates throw error', () => {
+    (fs.readdirSync as jest.Mock).mockReturnValue([TEST_ROOT_SR_TEMPLATE, TEST_ROOT_SR_TEMPLATE]);
+    (path.join as jest.Mock).mockReturnValue(TEST_ROOT_TEMPLATE_PATH);
+
+    (fs.statSync as jest.Mock).mockImplementation(() => {
+      return { isDirectory: () => false } as fs.Stats;
+    });
+
+    expect(() => findRulesFiles(TEST_DIR, TEST_ROOT_SR_TEMPLATE)).toThrow(
+      `Multiple SR Template files found: ${TEST_ROOT_SR_TEMPLATE}. Cannot proceed.`
+    );
+  });
+
+  test('find only .rules files and no template file then throw error', () => {
+    (fs.readdirSync as jest.Mock).mockReturnValue([TEST_FIRST_RULES_FILE, TEST_SECOND_RULES_FILE]);
+    (path.join as jest.Mock)
+      .mockReturnValueOnce(TEST_ROOT_TEMPLATE_PATH)
+      .mockReturnValueOnce(TEST_FIRST_RULES_FILE_PATH)
+      .mockReturnValueOnce(TEST_SECOND_RULES_FILE_PATH);
+    (fs.statSync as jest.Mock).mockImplementation(() => {
+      return { isDirectory: () => false } as fs.Stats;
+    });
+
+    expect(() => findRulesFiles(TEST_DIR, TEST_ROOT_SR_TEMPLATE)).toThrow(`Cannot find SR Template: ${TEST_ROOT_SR_TEMPLATE}.`);
+  });
+
+  test('find a root SR template file and no .rules files then throw error', () => {
+    (fs.readdirSync as jest.Mock).mockReturnValue([TEST_ROOT_SR_TEMPLATE]);
+    (path.join as jest.Mock).mockReturnValue(TEST_ROOT_TEMPLATE_PATH);
+    (fs.statSync as jest.Mock).mockImplementation(() => {
+      return { isDirectory: () => false } as fs.Stats;
+    });
+
+    expect(() => findRulesFiles(TEST_DIR, TEST_ROOT_SR_TEMPLATE)).toThrow('No rule file need to merged, exit.');
+  });
+
+  test('should find a root SR template and multiple .rules files in a directory', () => {
+    (fs.readdirSync as jest.Mock).mockReturnValue([TEST_ROOT_SR_TEMPLATE, TEST_FIRST_RULES_FILE, TEST_SECOND_RULES_FILE, TEST_FIRST_TS_FILE]);
+    (path.join as jest.Mock)
+      .mockReturnValueOnce(TEST_ROOT_TEMPLATE_PATH)
+      .mockReturnValueOnce(TEST_ROOT_TEMPLATE_PATH)
+      .mockReturnValueOnce(TEST_FIRST_RULES_FILE_PATH)
+      .mockReturnValueOnce(TEST_SECOND_RULES_FILE_PATH)
+      .mockReturnValueOnce(TEST_FIRST_TS_FILE_PATH);
+    (fs.statSync as jest.Mock).mockImplementation(() => {
+      return { isDirectory: () => false } as fs.Stats;
+    });
+
+    const result = findRulesFiles(TEST_DIR, TEST_ROOT_SR_TEMPLATE);
+
+    expect(result.rootSrTemplateFile).toBe(TEST_ROOT_TEMPLATE_PATH);
+    expect(result.subSrFiles).toEqual([TEST_FIRST_RULES_FILE_PATH, TEST_SECOND_RULES_FILE_PATH]);
+  });
+
+  test('should recursively find .rules files in subdirectories', () => {
+    (fs.readdirSync as jest.Mock).mockReturnValueOnce([TEST_ROOT_SR_TEMPLATE, TEST_SUB_DIR]).mockReturnValueOnce([TEST_FIRST_SUB_RULES_FILE, TEST_SECOND_SUB_RULES_FILE]);
+    (path.join as jest.Mock)
+      .mockReturnValueOnce(TEST_ROOT_TEMPLATE_PATH)
+      .mockReturnValueOnce(TEST_ROOT_TEMPLATE_PATH)
+      .mockReturnValueOnce(TEST_SUB_DIR_PARTH)
+      .mockReturnValueOnce(TEST_ROOT_TEMPLATE_PATH)
+      .mockReturnValueOnce(TEST_FIRST_SUB_RULES_FILE_PATH)
+      .mockReturnValueOnce(TEST_SECOND_SUB_RULES_FILE_PATH);
+
+    (fs.statSync as jest.Mock).mockImplementation((filePath) => {
+      if (filePath === TEST_SUB_DIR_PARTH) {
+        return { isDirectory: () => true } as fs.Stats;
+      } else {
+        return { isDirectory: () => false } as fs.Stats;
+      }
+    });
+
+    const result = findRulesFiles(TEST_DIR, TEST_ROOT_SR_TEMPLATE);
+
+    expect(result.rootSrTemplateFile).toBe(TEST_ROOT_TEMPLATE_PATH);
+    expect(result.subSrFiles).toEqual([TEST_FIRST_SUB_RULES_FILE_PATH, TEST_SECOND_SUB_RULES_FILE_PATH]);
+  });
+
+});
+
+describe('readFileContent', () => {
+  test('reading success', async () => {
+    const mockReadFile = (fs.promises.readFile as jest.Mock).mockImplementation(async () =>
+      Promise.resolve(TEST_FIRST_RULES_FILE_CONTENT)
+    );
+
+    const result = await readFileContent(TEST_FIRST_RULES_FILE_PATH);
+   
+    expect(result).toBe(TEST_FIRST_RULES_FILE_CONTENT);
+    expect(mockReadFile).toHaveBeenCalledWith(TEST_FIRST_RULES_FILE_PATH, 'utf-8');
+  });
+
+  test('should throw an error when reading the file fails', async () => {
+    const filePath = 'invalid.txt';
+
+    const mockReadFile = (fs.promises.readFile as jest.Mock).mockImplementation(async () =>
+      Promise.reject(new Error(`reading file failed: ${filePath}`))
+    );
+
+    await expect(readFileContent(filePath)).rejects.toThrow(
+      `reading file failed: ${filePath}`
+    );
+    expect(mockReadFile).toHaveBeenCalledWith(filePath, 'utf-8');
   });
 });
+
+// describe('Test mergeSecurityRules', () => {
+//   test('Test mergeSecurityRules success', () => {
+//     const expectedResult = 'example: merged security rules';
+//     const actualResult = mergeSecurityRules("a", 'b', "c");
+
+//     expect(actualResult).toEqual(expectedResult);
+//   });
+// });
--- src/merge-security-rules.ts
+++ src/merge-security-rules.ts
@@ -1,4 +1,88 @@
-export default function mergeSecurityRules() {
-  // TODO
+import fs, { Stats } from 'fs';
+import path from 'path';
+
+/**
+ * Scan the directory and its subdirectories for all '.rules' files
+ * 
+ * @param dir workspace path
+ * @param rootSrTemplate  root SR template file name
+ * @returns The path of all `.rules` files 
+ */
+export function findRulesFiles(dir: string, rootSrTemplate: string,isRootDir=true): { rootSrTemplateFile: string, subSrFiles: string[] } {
+  let results: string[] = [];
+  let rootSrTemplateFile: string = '';
+  const templatePath: string = path.join(dir,rootSrTemplate);
+  const list: string[] = fs.readdirSync(dir);
+
+  list.forEach(file => { 
+    const filePath: string = path.join(dir, file);
+    const stat: Stats = fs.statSync(filePath);
+
+    if (stat.isDirectory()) {
+      const { subSrFiles } = findRulesFiles(filePath, rootSrTemplate,false)
+      results = results.concat(subSrFiles);
+    } else if (filePath.endsWith('.rules')) {
+      results.push(filePath);
+    } else if (filePath === templatePath && isRootDir) {
+      if (rootSrTemplateFile) {
+        throw new Error(`Multiple SR Template files found: ${rootSrTemplate}. Cannot proceed.`)
+      }
+      rootSrTemplateFile = filePath
+    }
+  });
+
+  if (rootSrTemplateFile === '' && isRootDir) {
+    throw new Error(`Cannot find SR Template: ${rootSrTemplate}.`)
+  }
+  if (results.length === 0) {
+    throw new Error ('No rule file need to merged, exit.')
+  }
+  return { rootSrTemplateFile, subSrFiles: results };
+}
+
+/**
+ * reading specific file content
+ *  
+ * @param filePath file path
+ * @returns the certain file content
+ */
+export async function readFileContent(filePath: string): Promise<string> {
+  try {
+    return await fs.promises.readFile(filePath, 'utf-8');
+  } catch (error) {
+    throw new Error(`reading file failed: ${filePath} by ${error}`);
+  }
+}
+
+/**
+ * 
+ * @param workspacePath 
+ * @param rootSrTemplate 
+ * @param rootSrFile 
+ * @returns 
+ */
+export async function mergeSecurityRules(workspacePath: string, rootSrTemplate: string, rootSrFile: string) {
+  const { rootSrTemplateFile, subSrFiles } = findRulesFiles(workspacePath, rootSrTemplate);
+
+  const subSrContents: string[] = await Promise.all(
+    subSrFiles.map(file => readFileContent(file))
+  );
+
+  const mergedSubSrContent: string = subSrContents.join('\n');
+
+  const rootSrTemplateContent: string = await readFileContent(rootSrTemplateFile);
+
+  const finalContent: string = rootSrTemplateContent.replace(
+    /(\{[\s\S]*\})/,
+    (match) => {
+      return match.replace(
+        /(\})/,
+        `\n${mergedSubSrContent.trim()}\n$1`
+      );
+    }
+  );
+
+  fs.writeFileSync(path.join(workspacePath, rootSrFile), finalContent);
+
   return "example: merged security rules"
 }
