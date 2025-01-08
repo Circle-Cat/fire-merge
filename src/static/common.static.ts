c1cd739ce211b2a67c7535e20c68abbb6a93315b
吴雨 <wuyu@u.circlecat.org>
2025-01-08 04:38:09 +0000

    [FM-7] Implement readFileContent and mergeSecurityRules to handle merging of security rules
    Change-Id: I5f2d4e5c65de4f38995982160fbf5bc6845861da

--- src/static/common.static.ts
+++ src/static/common.static.ts
@@ -2,4 +2,6 @@ export class CommonStatic {
   public static readonly EMPTY_STRING = '';
   public static readonly RULES_FILE_SUFFIX = '.rules';
   public static readonly NO_ELEMENT = 0;
+  public static readonly ENCODING_UTF8 = 'utf-8';
+  public static readonly NEW_LINE_SYMBOL = '\n';
 }
