c7cca79e14687ce996c5537ecf787f997e54a880
Yanpei Wang <yuji@u.circlecat.org>
2025-01-09 04:15:04 +0000

    [FM-10] Add fetchCLIArguments method to handle CLI parameters
    - Introduced fetchCLIArguments function that accepts a callback as a parameter.
    - The function retrieves command-line arguments, validates them, and then calls the provided callback with the parsed parameters.
    
    Change-Id: I2e0bdc4c5088ab597ce10eff73a440c75f96e15c

--- src/static/common.static.ts
+++ src/static/common.static.ts
@@ -6,4 +6,11 @@ export class CommonStatic {
   public static readonly NEW_LINE_SYMBOL = '\n';
   public static readonly FUNCTION_CALLED_ONCE = 1;
   public static readonly FUNCTION_CALLED_THREE_TIMES = 3;
+  public static readonly CLI_ARG_WORKSPACE_PATH = '--workspace_path';
+  public static readonly CLI_ARG_ROOT_SR_TEMPLATE = '--root_sr_template';
+  public static readonly CLI_ARG_ROOT_SR_FILE = '--root_sr_file';
+  public static readonly INDEX_INCREMENT = 1;
+  public static readonly LOOP_START = 0;
+  public static readonly COMMAND_LINE_ARGS = 2;
+  public static readonly EXPECTED_ARG_COUNT = 6;
 }
--- src/static/errorMessage.static.ts
+++ src/static/errorMessage.static.ts
@@ -3,4 +3,7 @@ export class ErrorMessageStatic {
   public static readonly SR_TEMPLATE_NOT_FOUND_ERROR = 'Cannot find SR Template: {rootSrTemplate}.';
   public static readonly NO_RULE_FILE_TO_MERGE = 'No rule file need to merged, exit.';
   public static readonly FILE_READ_FAILED_ERROR = 'Reading file failed: {filePath} by {error}';
+  public static readonly REQUIRED_PARAMETERS_MISSING_MESSAGE = 'Missing required parameters.';
+  public static readonly REQUIRED_PARAMETERS_UNEXPECTED_MESSAGE = 'Unexpected argument: ';
+  public static readonly REQUIRED_PARAMETERS_DUPLICATED_MESSAGE = 'Duplicate argument found';
 }
