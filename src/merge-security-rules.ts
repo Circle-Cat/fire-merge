348718867cdc4a1e6f6c90ccbafe8eefa0184f52
Yanpei Wang <yuji@u.circlecat.org>
2025-01-21 06:17:48 +0000

    [FM-10] Add fetchCLIArguments method to handle CLI parameters
    - Introduced fetchCLIArguments function that accepts a callback as a parameter.
    - The function retrieves command-line arguments, validates them, and then calls the provided callback with the parsed parameters.
    
    Change-Id: I677a0a0083c1864a3e1dd0bcbba255d96e977669

--- src/merge-security-rules.ts
+++ src/merge-security-rules.ts
@@ -1,48 +1,132 @@
 import fs, { Stats } from 'fs';
 import path from 'path';
 import { CommonStatic } from './static/common.static';
-import {ErrorMessageStatic} from './static/errorMessage.static'
+import { ErrorMessageStatic } from './static/errorMessage.static';
+import * as commander from "commander";
 
+const REGEX_CONTENT_WITH_TRAILING_CLOSING_BRACES = /(.*)(\s*\}\s*\}\s*)$/;
 /**
  * Scan the directory and its subdirectories for all '.rules' files
- * 
+ *
  * @param dir workspace path
  * @param rootSrTemplate  root SR template file name
- * @returns The path of all `.rules` files 
+ * @returns The path of all `.rules` files
  */
-export function findRulesFiles(dir: string, rootSrTemplate: string,isRootDir=true): { rootSrTemplateFile: string, subSrFiles: string[] } {
+export function findRulesFiles(dir: string, rootSrTemplate: string, isRootDir = true): { rootSrTemplateFile: string, subSrFiles: string[] } {
   let results: string[] = [];
   let rootSrTemplateFile: string = CommonStatic.EMPTY_STRING;
-  const templatePath: string = path.join(dir,rootSrTemplate);
+  const templatePath: string = path.join(dir, rootSrTemplate);
   const list: string[] = fs.readdirSync(dir);
 
-  list.forEach(file => { 
+  list.forEach(file => {
     const filePath: string = path.join(dir, file);
     const stat: Stats = fs.statSync(filePath);
 
     if (stat.isDirectory()) {
-      const { subSrFiles } = findRulesFiles(filePath, rootSrTemplate,false);
+      const { subSrFiles } = findRulesFiles(filePath, rootSrTemplate, false);
       results = results.concat(subSrFiles);
     } else if (filePath.endsWith(CommonStatic.RULES_FILE_SUFFIX)) {
       results.push(filePath);
     } else if (filePath === templatePath && isRootDir) {
       if (rootSrTemplateFile) {
-        throw new Error(ErrorMessageStatic.MULTIPLE_SR_TEMPLATE_FILES_ERROR.replace('{rootSrTemplate}',rootSrTemplate));
+        throw new Error(ErrorMessageStatic.MULTIPLE_SR_TEMPLATE_FILES_ERROR.replace('{rootSrTemplate}', rootSrTemplate));
       }
       rootSrTemplateFile = filePath;
     }
   });
 
   if (rootSrTemplateFile === CommonStatic.EMPTY_STRING && isRootDir) {
-    throw new Error(ErrorMessageStatic.SR_TEMPLATE_NOT_FOUND_ERROR.replace('{rootSrTemplate}',rootSrTemplate));
+    throw new Error(ErrorMessageStatic.SR_TEMPLATE_NOT_FOUND_ERROR.replace('{rootSrTemplate}', rootSrTemplate));
   }
   if (results.length === CommonStatic.NO_ELEMENT) {
-    throw new Error (ErrorMessageStatic.NO_RULE_FILE_TO_MERGE);
+    throw new Error(ErrorMessageStatic.NO_RULE_FILE_TO_MERGE);
   }
   return { rootSrTemplateFile, subSrFiles: results };
 }
 
-// export default function mergeSecurityRules() {
-//   // TODO
-//   return "example: merged security rules"
-// }
+/**
+ * reading specific file content
+ *
+ * @param filePath file path
+ * @returns the certain file content
+ */
+export async function readFileContent(filePath: string): Promise<string> {
+  try {
+    return await fs.promises.readFile(filePath, CommonStatic.ENCODING_UTF8);
+  } catch (error) {
+    throw new Error(ErrorMessageStatic.FILE_READ_FAILED_ERROR.replace('{filePath}', filePath).replace('{error}', (error as Error).toString()));
+  }
+}
+
+/**
+ * Merges the contents of multiple sub-rule files with a specified template file and generates a new root rule file.
+ * If the root rule file already exists, it will be overwritten.
+ *
+ * @param workspacePath - The repository path where the security rule files are located.
+ * @param rootSrTemplate - The file name of the root security rule template that will be used as the base.
+ * @param rootSrFile - The name of the output file where the final merged security rules will be saved. 
+ *If this file exists, it will be overwritten.
+ */
+export async function mergeSecurityRules(workspacePath: string, rootSrTemplate: string, rootSrFile: string) {
+  const { rootSrTemplateFile, subSrFiles } = findRulesFiles(workspacePath, rootSrTemplate);
+
+  const subSrContents: string[] = await Promise.all(
+    subSrFiles.map((file: string) => readFileContent(file))
+  );
+
+  const mergedSubSrContent: string = subSrContents.join(CommonStatic.NEW_LINE_SYMBOL);
+
+  const rootSrTemplateContent: string = await readFileContent(rootSrTemplateFile);
+
+  const finalContent: string = rootSrTemplateContent.replace(
+    REGEX_CONTENT_WITH_TRAILING_CLOSING_BRACES,
+    (_, start, end) => {
+      const cleanedSubRules = mergedSubSrContent
+        .split(CommonStatic.NEW_LINE_SYMBOL)
+        .map(line => line.trim() ? `    ${line}` : CommonStatic.EMPTY_STRING)
+        .join(CommonStatic.NEW_LINE_SYMBOL)
+      return `${start}\n${cleanedSubRules}${end}`;
+    }
+  );
+  fs.writeFileSync(path.join(workspacePath, rootSrFile), finalContent);
+}
+
+/**
+ * This function processes command line arguments passed to the script and 
+ * calls the provided callback with specific arguments extracted from the command line.
+ * It looks for three specific arguments:
+ * - `--workspace_path`: The repository path where the security rule files are located.
+ * - `--root_sr_template`: The file name of the root security rule template that will be used as the base.
+ * - `--root_sr_file`: The name of the output file where the final merged security rules will be saved.
+ * 
+ * The function checks if the required parameters are provided. If any of the parameters 
+ * are missing, it logs an error and exits without calling the callback.
+ * 
+ * @param callback - The callback function to be invoked once the command line 
+ * arguments are successfully parsed. The callback should accept three parameters:
+ *  - `workspacePath` (string)
+ *  - `rootSrTemplate` (string)
+ *  - `rootSrFile` (string)
+ * 
+ * @returns {Promise<void>} A promise that resolves when the callback is called with the parsed arguments.
+ * If required arguments are missing, the promise will not resolve, and the function will log an error.
+ * 
+ * @throws {Error} If any of the required command line arguments are missing, an error message will be logged.
+ * 
+ */
+export async function fetchCLIArguments(
+  callback: (workspacePath: string, rootSrTemplate: string, rootSrFile: string) => Promise<void>,
+): Promise<void> {
+  const program = new commander.Command();
+
+  program
+    .requiredOption('--workspace_path <path>', 'The repository path where the security rule files are located')
+    .requiredOption('--root_sr_template <path>', 'The root security rule template file name')
+    .requiredOption('--root_sr_file <path>', 'The output file name for merged security rules')
+    .action(async () => {
+      const { workspace_path, root_sr_template, root_sr_file } = program.opts();
+      callback(workspace_path, root_sr_template, root_sr_file);
+    });
+
+  program.parse(process.argv);
+}
