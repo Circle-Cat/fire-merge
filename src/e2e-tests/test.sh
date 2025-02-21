0d8e42ac13a0f3f34f57ada9eda69c67b3ab53ca
吴雨 <wuyu@u.circlecat.org>
2025-02-21 01:26:30 +0000

    DRAFT
    Change-Id: I2481b571ff19e83f6b46b6882bf035375425222e

---
+++ src/e2e-tests/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WORKSPACE_PATH="../src/e2e-tests/workspace"
+TEMPLATE_FILE="firestore.rules.template"
+OUTPUT_FILE="firestore.rules"
+
+rm -f "$WORKSPACE_PATH/$OUTPUT_FILE"
+
+echo "Running Test 1: Successful Merge"
+#node ../fire-mergets/src/index.ts --workspace_path "$WORKSPACE_PATH" --root_sr_template "$TEMPLATE_FILE" --root_sr_file "$OUTPUT_FILE"
+node ../fire-merge/dist/index.js --workspace_path "$WORKSPACE_PATH" --root_sr_template "$TEMPLATE_FILE" --root_sr_file "$OUTPUT_FILE"
+if [ -f "$WORKSPACE_PATH/$OUTPUT_FILE" ]; then
+  echo "Test 1 Passed: File $OUTPUT_FILE generated successfully."
+else
+  echo "Test 1 Failed: $OUTPUT_FILE not generated."
+fi
